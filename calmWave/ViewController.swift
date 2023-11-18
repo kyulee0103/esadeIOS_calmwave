6e1d22402bfe8fa148ac48fb6732d4ad5671a04b
kyulee0103 <kyulee1338@gmail.com>
2023-11-18 23:25:44 +0100

    Initial Commit

---
+++ calmWave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calmWave
+//
+//  Created by 권규리 on 2023/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
